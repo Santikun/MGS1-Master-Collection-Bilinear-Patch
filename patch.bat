8b8179d1838d6b3b5a2dcfbeb3df56e7cd3d9415
Santikun <santi_leo_2007@hotmail.com>
2023-10-31 09:01:25 -0300

    Some changes made to the .bat file

--- patch.bat
+++ patch.bat
@@ -4,7 +4,7 @@ cls
 echo ===BILINEAR FILTERING REMOVER===
 echo.
 
-echo BACK UP YOUR ORIGINAL FILE BEFORE DOING THIS !!!!
+echo BACK UP YOUR ORIGINAL .psb AND .psb.m FILES BEFORE DOING THIS !!!!
 echo Drag and drop your alldata.bin file and press enter
 Set /p binpath=
 echo.
@@ -12,7 +12,7 @@ echo.
 echo Patching, please wait...
 start "" /wait "%~dp0MArchiveBatchTool\MArchiveBatchTool.exe" archive extract --codec zlib --seed  25G/xpvTbsb+6 --keyLength 64 "%binpath%" "%temp%\bilinearpatcher"
 echo First step complete. Step 2...
-xcopy /y "%~dp0systemdata_setting_screen.nut.m" "%temp%\bilinearpatcher\system\script"
+copy /y "%~dp0bili.nut.m" "%temp%\bilinearpatcher\system\script\systemdata_setting_screen.nut.m"
 echo Second step complete. Step 3...
 start "" /wait "%~dp0MArchiveBatchTool\MArchiveBatchTool.exe" archive build --codec zlib --seed  25G/xpvTbsb+6 --keyLength 64 "%temp%\bilinearpatcher" "%binpath:~0,-4%"
 echo Third step complete. Cleaning files...
